0a0991482eff452a7a9d63d572ffa3c95f62bb05
Steve Babb <steve.babb@outlook.com>
2021-03-11 15:36:26 -0600

    connection to heroku

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@
 
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
+const connection = mysql.createConnection(process.env.JAWSDB_URL|| {
     host: 'localhost',
 
     port: 3306,
